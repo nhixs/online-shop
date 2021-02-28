030e56d6eb326ab6ed76c0edbdfc43d426fb6fe8
nhixs <60426938+nhixs@users.noreply.github.com>
2021-02-28 14:28:09 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,13 +1,12 @@
 <?php 
 
 $koneksi = mysqli_connect(
-    "localhost",
-    "root",
-    "" ,
-    "project_tokoonline");
+    "https://www.db4free.net/",
+    "nhisx_skirpsi@localhost",
+    "bagus246" ,
+    "elok_cookies");
 
     // Check connection
     if (mysqli_connect_errno()){
 	echo "Koneksi database gagal : " . mysqli_connect_error();
 }
-    ?>
